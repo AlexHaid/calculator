588a5408b3377e78f14f0d071ea7d02195ae4588
AlexHaid <alexhaid95@gmail.com>
2019-03-30 20:08:50 +0300

    Resolved PR 29.03

--- calc.js
+++ calc.js
@@ -3,68 +3,65 @@ const operationButtons = document.querySelectorAll('.operation');
 const resultButton = document.querySelector('.result');
 const workField = document.querySelector('.workfield');
 let isSecondOperand = false;
+let {value} = workField;
+let firstOperand = null;
+let secondOperand = null;
+let operationType = null;
+let result = null;
 const calculate = () => {
-	let firstOperand = null;
-	let secondOperand = null;
-	let operationType = null;
-	let result = null;
-	let {value} = workField;
-
-	const inputOperand = () => {
-		if (!isSecondOperand) {
-			firstOperand = +value;
-			isSecondOperand = true;
-		} else {
-			secondOperand = +value;
-		}
-	}
-
-	const typeNumber = (event) => {
-		const isFirstOperand = !+value || value == firstOperand;
-		let {target: {innerText}} = event;
-		value = !isFirstOperand ? value + innerText : '' + innerText;
-		workField.value = value;		
-	};
-
 	numberButtons.forEach((num) => { 
 		num.addEventListener('click', typeNumber);
 	});
-
-	const getResult = () => {
-		inputOperand();
-		switch (operationType) {
-			case '+' : 
-				result = firstOperand + secondOperand;	
-				break;
-			case '-' : 
-				result = firstOperand - secondOperand;
-				break;
-			case '*' : 
-				result = firstOperand * secondOperand;
-				break;
-			case '/' : 
-				result = firstOperand / secondOperand;
-				break;
-			default:
-				result = 'Error!';
-		}
-		return result;	
-	};
-	const registerOperand = () => {
-		inputOperand();
-		let {target: {dataset: {operation}}} = event;
-		operationType = event.target.dataset.operation;
-	}
  	operationButtons.forEach(operation => {
 		operation.addEventListener('click', registerOperand);	
 	});
-
-	const formResult = () => {
-		getResult();
-		isSecondOperand = false;
-		workField.value = value = result;
-	}
 	resultButton.addEventListener('click', formResult);
+}
+
+const typeNumber = (event) => {
+	const isFirstOperand = !+value || value == firstOperand;
+	let {target: {innerText}} = event;
+	value = !isFirstOperand ? value + innerText : innerText;
+	workField.value = value;
+	return value;
+};
+
+const inputOperand = () => {
+	!isSecondOperand ? (firstOperand = +value, isSecondOperand = true) : secondOperand = +value;
+}
+
+const registerOperand = () => {
+	inputOperand();
+	let {target: {dataset: {operation}}} = event;
+	operationType = operation;
+	return operationType;
+}
+
+const getResult = () => {
+	inputOperand();
+	switch (operationType) {
+		case '+' : 
+			result = firstOperand + secondOperand;	
+			break;
+		case '-' : 
+			result = firstOperand - secondOperand;
+			break;
+		case '*' : 
+			result = firstOperand * secondOperand;
+			break;
+		case '/' : 
+			result = firstOperand / secondOperand;
+			break;
+		default:
+			result = 'Error!';
+	}
+	return result;	
+};
+
+const formResult = () => {
+	getResult();
+	isSecondOperand = false;
+	workField.value = value = result;
 }
 
 calculate();
