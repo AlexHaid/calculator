1e345171b2bd19d86d0f0d1facdc7b967f35fe8f
Alex Haid <alexhaid95gmail.com>
2019-04-30 20:14:52 +0300

    refactor: parallel functions

--- calc.js
+++ calc.js
@@ -6,84 +6,62 @@ const initCalculator = () => {
 	const plusMinus = document.querySelector('.buttons-container__change-sign');
 	const clearButton = document.querySelector('.buttons-container__clear-current');
 	const clearAllButton = document.querySelector('.buttons-container__clear-all');
-
 	let firstNumber = null;
 	let secondNumber = null;
 	let operationType = null;
-	let isFirstDigit = true;
-	let isFirstNumberPriority = true;
 
-	const displayDigit = () => {
-		const getDigit = e => e.target.innerText;
-		if (isFirstDigit) {
-			isFirstDigit = false;
-			inputField.value = '';
-		}
-		inputField.value += getDigit(event);
-	};
-
-	const clear = () => {
-		if (event.target == clearAllButton) {
-			firstNumber = 0;
-			secondNumber = 0;
-			isFirstNumberPriority = true;
-		}
-		inputField.value = 0;
-		isFirstDigit = true;
-	};
-
-	const displayNumber = () => {
-		const getNumber = () => {
-			let {value} = inputField;
-			isFirstDigit = true;
-			return +value;
-		};
-		(isFirstNumberPriority && !firstNumber || !isFirstNumberPriority) ? firstNumber = getNumber() : secondNumber = getNumber();
-	};
-
-	const getOperation = () => {
-		let {target: {dataset: {operation}}} = event;
-		operationType = operation;
-		isFirstNumberPriority = true;
-	};
-
-	const changeSign = () => {
-		let number = +inputField.value;
-		number = -number;
-		inputField.value = number;
-	}
-
-	const getResult = () => {
-		displayNumber();
-		const sum = (a,b) => a + b;
-		const distract = (a,b) => a - b;
-		const multiply = (a,b) => a * b;
-		const divide = (a,b) => a / b;
-		const arithmetic = {
-			'+' : sum(firstNumber, secondNumber),
-			'-' : distract(firstNumber, secondNumber),
-			'*' : multiply(firstNumber, secondNumber),
-			'/' : divide(firstNumber, secondNumber),
-		}
-		isFirstNumberPriority = false;
-		return arithmetic[operationType];
-	};
-
-	const displayResult = () => {
-		inputField.value = getResult();
-	};
-
+	
 	numberButtons.forEach((num) => { 
 		num.addEventListener('click', displayDigit);
 	});
  	operationButtons.forEach(operation => {
-		operation.addEventListener('click', getOperation);	
-		operation.addEventListener('click', displayNumber);	
+		operation.addEventListener('click', () => {
+			operationType = getOperation();
+			firstNumber = getNumber();
+		});
 	});
-	plusMinus.addEventListener('click', changeSign);
-	resultButton.addEventListener('click', displayResult);
-	clearButton.addEventListener('click', clear);
-	clearAllButton.addEventListener('click', clear);
+
+	resultButton.addEventListener('click', () => {
+		secondNumber = getNumber();
+		inputField.value = getResult(firstNumber, secondNumber, operationType);
+	});
+};
+
+const displayDigit = () => {
+	const inputField = document.querySelector('.calculator__inputfield');
+	let {dataset: {readyToClear}} = inputField;
+	const getDigit = e => e.target.innerText;
+	if (!+inputField.value || readyToClear == 'true') {
+		inputField.value = '';
+		inputField.dataset.readyToClear = false;
+	};
+	inputField.value += getDigit(event);
+};
+
+const getOperation = () => {
+	let {target: {dataset: {operation}}} = event;
+	const inputField = document.querySelector('.calculator__inputfield');
+	inputField.dataset.readyToClear = true;
+	return operation;
+};
+
+const getNumber = () => {
+	const inputField = document.querySelector('.calculator__inputfield');
+	return +inputField.value;
+};
+
+const getResult = (first, second, op) => {
+	const sum = (a,b) => a + b;
+	const distract = (a,b) => a - b;
+	const multiply = (a,b) => a * b;
+	const divide = (a,b) => a / b;
+	const arithmetic = {
+		'+' : sum(first, second),
+		'-' : distract(first, second),
+		'*' : multiply(first, second),
+		'/' : divide(first, second),
+	}
+	return arithmetic[op];
 };
 
 initCalculator();
